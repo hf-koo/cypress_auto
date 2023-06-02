7da675fa7a3b9dbec0e3bde8fe8a3ea0649b8374
HungFai Koo <jhk888@yahoo.com>
2023-06-02 14:03:34 -0700

    second commit

---
+++ cypress/e2e/contact-us.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress"/>
+
+describe("Test contact us from webdriverUniversity ", () => {
+  it("should be able to access the website", () => {
+    cy.visit("https://webdriveruniversity.com/");
+  });
+
+  it("should be able to access the contact us page", () => {
+    cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html");
+  });
+});
