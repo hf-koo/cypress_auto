27fed9651fa836d6eadaaf81475117aebae30764
HungFai Koo <jhk888@yahoo.com>
2023-06-02 21:20:36 -0700

    first challenge completed

--- cypress/e2e/contact-us.cy.js
+++ cypress/e2e/contact-us.cy.js
@@ -1,16 +1,21 @@
 /// <reference types="Cypress"/>
 
 describe("Test contact us from webdriverUniversity ", () => {
-  it("should be able to access the website", () => {
+  xit("should be able to access the website", () => {
     cy.visit("https://webdriveruniversity.com/");
   });
 
-  it("should be able to access the contact us page", () => {
+  xit("should be able to access the contact us page", () => {
     cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html");
   });
 
   it("should be able to access first name page", () => {
     cy.visit("https://webdriveruniversity.com/Contact-Us/contactus.html");
     cy.get('[name="first_name"]').type("Jacky");
+    cy.get('[name="last_name"]').type("Koo");
+    cy.get('[name="email"]').type("123@mail.com");
+    cy.get("textarea.feedback-input").type("I just ate dessert!");
+    cy.get('[type="submit"]').click();
+    cy.contains("Thank You for your Message!");
   });
 });
