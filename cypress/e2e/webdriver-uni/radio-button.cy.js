0ecab61e0a04c8fba2603ee9110286776b5227e9
HungFai Koo <jhk888@yahoo.com>
2023-07-16 16:16:42 -0700

    radioButtons check and unchecked

---
+++ cypress/e2e/webdriver-uni/radio-button.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="Cypress"/>
+
+describe("Verify radion buttons via webdriveruni", () => {
+  it("check specific radio buttons", () => {
+    cy.visit("https://webdriveruniversity.com/");
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.get("#radio-buttons").find("[type='radio']").first().check();
+    cy.get("#radio-buttons").find("[type='radio']").eq(1).check();
+  });
+  it("Validate the states of specific radio buttons", () => {
+    cy.visit("https://webdriveruniversity.com/");
+    cy.get("#dropdown-checkboxes-radiobuttons")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.get("[value='lettuce']").should("not.be.checked");
+    // cy.get("[value='cabbage']").should("be.checked");
+    cy.get("[value='pumpkin']").should("be.checked");
+
+    cy.get("[value='lettuce']").check();
+    cy.get("[value='pumpkin']").should("not.be.checked");
+    cy.get("[value='pumpkin']").check();
+    cy.get("[value='lettuce']").should("not.be.checked");
+
+    cy.get('[value="cabbage"]').should("be.disabled");
+  });
+});
