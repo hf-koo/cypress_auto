faf7037cad2bf003c451d568956698a0cf404728
HungFai Koo <jhk888@yahoo.com>
2023-07-17 15:43:36 -0700

    autocomplete dropdown list

---
+++ cypress/e2e/webdriver-uni/autocomplete-dropdown-list.cy.js
@@ -0,0 +1,39 @@
+/// <reference types="Cypress"/>
+
+describe("Vertify Autocomplete dropdown lists via webdriveruni", () => {
+  it("Select specific product via autocomplete list", () => {
+    cy.visit("https://webdriveruniversity.com/");
+    cy.get("#autocomplete-textfield")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+    cy.get("#myInput").type("A");
+
+    cy.get("#myInputautocomplete-list >*")
+      .each(($el, index, $list) => {
+        const prod = $el.text();
+        const productToSelect = "Avacado";
+
+        if (prod === productToSelect) {
+          $el.trigger("click");
+
+          cy.get("#submit-button").click();
+          cy.url().should("include", productToSelect);
+        }
+      })
+      .then(() => {
+        cy.get("#myInput").type("g");
+
+        cy.get("#myInputautocomplete-list >*").each(($el, index, $list) => {
+          const prod = $el.text();
+          const productToSelect = "Grapes";
+
+          if (prod === productToSelect) {
+            $el.trigger("click");
+
+            cy.get("#submit-button").click();
+            cy.url().should("include", productToSelect);
+          }
+        });
+      });
+  });
+});
